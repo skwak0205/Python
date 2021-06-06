50c22f44b9ebba5867e2328d75c9d8f5bf7718cc
Soojung Kwak <skwak0205@gmail.com>
2021-06-06 13:45:34 +0900

    파이썬 튜토리얼 - list

---
+++ tutorial/list.py
@@ -0,0 +1,41 @@
+# 리스트 [] : 순서를 가지는 객체의 집합
+subway = [10, 20, 30]
+print(subway)
+
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
+
+print(subway.index("조세호"))
+
+subway.append("하하")
+print(subway)
+
+subway.insert(1, "정형돈")
+
+# 뒤에서 하나씩 꺼냄
+print(subway.pop())
+
+# 같은 이름의 사람이 몇 명 있는지 확인
+subway.append("유재석")
+print(subway.count("유재석"))
+
+# 정렬
+num_list = [5, 2, 4, 3, 1]
+num_list.sort()
+print(num_list)
+
+# 순서 뒤집기
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기
+num_list.clear()
+print(num_list)
+
+# 다양한 자료형 함께 사용
+mix_list = ["조세호", 20, True]
+print(mix_list)
+
+# 리스트 확장
+num_list.extend(mix_list)
+print(num_list)
