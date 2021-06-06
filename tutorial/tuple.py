67cc3f8b1fcaf266310f8c1f4c47ebbe7ede6717
Soojung Kwak <skwak0205@gmail.com>
2021-06-06 13:45:50 +0900

    파이썬 튜토리얼 - tuple

---
+++ tutorial/tuple.py
@@ -0,0 +1,17 @@
+# 내용 변경 / 추가 불가능
+# 속도가 빠름
+
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+menu.add("생선까스") # 에러 발생
+
+name = "김종국"
+age = 20
+hobby = "코딩"
+print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
+
